c42186c3f8417a785e0549b005ece7e257ecb95d
Jurell Afable <133481515+afbl-11@users.noreply.github.com>
2025-10-13 00:55:08 +0800

    WIP: removed unncessary components

--- resources/views/components/button-image.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['type' => 'submit', 'icon' => null])
-
-<button type="{{$type}}" {{$attributes->merge(['class' => 'button-image'])}}>
-    @if($icon)
-    <img src="{{ asset($icon) }}" alt="icon" class="w-6 h-6">
-    @endif
-    {{$slot}}
-</button>
