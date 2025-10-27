2e67be31fd6eb06ce82ae64e758adcf73ba82d9d
Jurell Afable <133481515+afbl-11@users.noreply.github.com>
2025-10-28 01:33:27 +0800

    refactor: added hidden input fields, sending name value to db

--- resources/js/api/address-picker.js
+++ resources/js/api/address-picker.js
@@ -1,81 +1,109 @@
-import {getRegions, getCities,getProvinces,getBarangays} from "./psgc.js";
+import { getRegions, getProvinces, getCities, getBarangays } from "./psgc.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     const regionSelect = document.getElementById("region");
-    const citySelect = document.getElementById("city");
     const provinceSelect = document.getElementById("province");
+    const citySelect = document.getElementById("city");
     const barangaySelect = document.getElementById("barangay");
 
-//     load regions
-    const  regions = await getRegions();
+    // hidden name inputs
+    const regionNameInput = document.getElementById("region_name");
+    const provinceNameInput = document.getElementById("province_name");
+    const cityNameInput = document.getElementById("city_name");
+    const barangayNameInput = document.getElementById("barangay_name");
+
+    function createOption(code, name) {
+        const option = document.createElement("option");
+        option.value = code;           // keep code so nested fetch endpoints work
+        option.textContent = name;
+        option.setAttribute("data-name", name);
+        return option;
+    }
+
+    // load regions
+    const regions = await getRegions();
     regions.forEach(region => {
-       const option = document.createElement("option");
-       option.value = region.code;
-       option.textContent = region.name;
-        option.setAttribute("data-name", region.name);
-       regionSelect.appendChild(option);
+        regionSelect.appendChild(createOption(region.code, region.name));
     });
 
-//on region change province
+
     regionSelect.addEventListener("change", async () => {
+
         provinceSelect.innerHTML = "<option value=''>Select Province</option>";
-        citySelect.innerHTML = "<option value=''>Select City / Municipality</option> ";
-        barangaySelect.innerHTML = "<option value=''>Select Barangay</option> ";
+        citySelect.innerHTML = "<option value=''>Select City / Municipality</option>";
+        barangaySelect.innerHTML = "<option value=''>Select Barangay</option>";
+        provinceNameInput.value = "";
+        cityNameInput.value = "";
+        barangayNameInput.value = "";
 
-        if(!regionSelect.value) return;
+        // set region name hidden input
+        const selectedRegionOption = regionSelect.options[regionSelect.selectedIndex];
+        regionNameInput.value = selectedRegionOption?.dataset?.name || "";
 
-        // load province
+        if (!regionSelect.value) return;
+
+        // load provinces by region code
         const provinces = await getProvinces(regionSelect.value);
-        provinces.forEach(province => {
-            const option = document.createElement("option");
-            option.value = province.code;
-            option.textContent = province.name;
-            option.setAttribute("data-name", province.name);
-            provinceSelect.appendChild(option);
+        provinces.forEach(prov => {
+            provinceSelect.appendChild(createOption(prov.code, prov.name));
         });
     });
-    regionSelect.addEventListener("change", async () => {
-        console.log("Selected region code:", regionSelect.value);
-        const provinces = await getProvinces(regionSelect.value);
-        console.log("Loaded provinces:", provinces);
-    });
 
-//     on province change city
+
     provinceSelect.addEventListener("change", async () => {
-        citySelect.innerHTML = "<option value=''>Select City / Municipality</option> ";
-        barangaySelect.innerHTML = "<option value=''>Select Barangay</option> ";
+        citySelect.innerHTML = "<option value=''>Select City / Municipality</option>";
+        barangaySelect.innerHTML = "<option value=''>Select Barangay</option>";
+        cityNameInput.value = "";
+        barangayNameInput.value = "";
+
+        const selectedProvinceOption = provinceSelect.options[provinceSelect.selectedIndex];
+        provinceNameInput.value = selectedProvinceOption?.dataset?.name || "";
 
         if (!provinceSelect.value) return;
 
-    //     load city
         const cities = await getCities(provinceSelect.value);
         cities.forEach(city => {
-            const option = document.createElement("option");
-            option.value = city.code;
-            option.textContent = city.name;
-            option.setAttribute("data-name", city.name);
-            citySelect.appendChild(option);
+            citySelect.appendChild(createOption(city.code, city.name));
         });
     });
 
-//     on city change barangay
+
     citySelect.addEventListener("change", async () => {
-        barangaySelect.innerHTML = "<option value=''>Select Barangay</option> ";
+        barangaySelect.innerHTML = "<option value=''>Select Barangay</option>";
+        barangayNameInput.value = "";
 
-        if(!citySelect.value) return;
+        const selectedCityOption = citySelect.options[citySelect.selectedIndex];
+        cityNameInput.value = selectedCityOption?.dataset?.name || "";
 
-    //     load barangay
+        if (!citySelect.value) return;
+
         const barangays = await getBarangays(citySelect.value);
-        barangays.forEach(barangay => {
-            const option = document.createElement("option");
-            option.value = barangay.code;
-            option.textContent = barangay.name;
-            option.setAttribute("data-name", barangay.name);
-            barangaySelect.appendChild(option);
+        barangays.forEach(b => {
+            barangaySelect.appendChild(createOption(b.code, b.name));
         });
+    });
+
+    barangaySelect.addEventListener("change", () => {
+        const selectedBarangayOption = barangaySelect.options[barangaySelect.selectedIndex];
+        barangayNameInput.value = selectedBarangayOption?.dataset?.name || "";
+    });
+
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
+        // region
+        const rOpt = regionSelect.options[regionSelect.selectedIndex];
+        regionNameInput.value = regionNameInput.value || rOpt?.dataset?.name || rOpt?.text || "";
+
+        // province
+        const pOpt = provinceSelect.options[provinceSelect.selectedIndex];
+        provinceNameInput.value = provinceNameInput.value || pOpt?.dataset?.name || pOpt?.text || "";
+
+        // city
+        const cOpt = citySelect.options[citySelect.selectedIndex];
+        cityNameInput.value = cityNameInput.value || cOpt?.dataset?.name || cOpt?.text || "";
+
+        // barangay
+        const bOpt = barangaySelect.options[barangaySelect.selectedIndex];
+        barangayNameInput.value = barangayNameInput.value || bOpt?.dataset?.name || bOpt?.text || "";
     });
 });
-
-//TODO: change stored value from code to name
-//example: region code: 000022002 to eastern samar
-// TODO:
