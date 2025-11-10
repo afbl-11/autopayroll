067e737c86835563f74714a3b6004e1e834b31f8
Jurell Afable <133481515+afbl-11@users.noreply.github.com>
2025-11-11 00:52:06 +0800

    feat: created migration and model

---
+++ app/Models/Announcement.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Announcement extends Model
+{
+    protected $table = 'announcements';
+    public $incrementing = false;
+    protected $primaryKey = 'announcement_id';
+
+    protected $fillable = [
+        'announcement_id',
+        'admin_id',
+        'title',
+        'message',
+        'start_date',
+        'end_date',
+        'created_by',
+        'is_active',
+    ];
+
+    public function admin(){
+        return $this->belongsTo(Admin::class, 'admin_id', 'admin_id');
+    }
+}
