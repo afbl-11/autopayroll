598bf9d1ff36f2b2abc2f823231249fe68e62505
Jurell Afable <133481515+afbl-11@users.noreply.github.com>
2025-11-15 00:25:16 +0800

    feat: rate logic

---
+++ app/Services/Payroll/EmployeeRatesService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services\Payroll;
+
+use App\Models\Employee;
+use App\Models\EmployeeRate;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+
+class EmployeeRatesService
+{
+    public function createRate(array $data) : EmployeeRate {
+
+        $employee = Employee::find($data['employeeId']);
+
+        return EmployeeRate::create([
+           'employee_rate_id' => Str::uuid(),
+            'employee_id' => $data['employeeId'],
+            'admin_id' => $employee->admin_id,
+            'rate' => $data['rate'],
+            'effective_from' => Carbon::now()->format('Y-m-d'),
+        ]);
+    }
+
+    public function updateRate(array $data) : EmployeeRate {
+
+        $oldRate = EmployeeRate::where('employee_id', $data['employeeId'])
+                ->whereNull('effective_to')
+                ->first();
+
+        $oldRate->update([
+            'effective_to' => Carbon::now()->sub(1,'day')->format('Y-m-d'),
+        ]);
+
+        return $this->createRate($data['employeeId']);
+    }
+}
