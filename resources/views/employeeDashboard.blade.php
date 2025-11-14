995a9db4ab79b3998495504a805013364e261b45
Jurell Afable <133481515+afbl-11@users.noreply.github.com>
2025-11-15 01:40:54 +0800

    fix:temporary removed filters

--- resources/views/employeeDashboard.blade.php
+++ resources/views/employeeDashboard.blade.php
@@ -1,36 +1,36 @@
 @vite(['resources/css/employee_dashboard/employeeDashboard.css'])
 <x-app :title="$title">
     <nav>
-        <div class="nav-group">
-            <x-form-input
-                type="search"
-                name="search_bar"
-                id="search_bar"
-                placeholder="Search Employee"
-                class="nav-items"
-                :noDefault="true"
-                :value="request('search_bar')"
-            ></x-form-input>
+{{--        <div class="nav-group">--}}
+{{--            <x-form-input--}}
+{{--                type="search"--}}
+{{--                name="search_bar"--}}
+{{--                id="search_bar"--}}
+{{--                placeholder="Search Employee"--}}
+{{--                class="nav-items"--}}
+{{--                :noDefault="true"--}}
+{{--                :value="request('search_bar')"--}}
+{{--            ></x-form-input>--}}
 
-            <div class="filters">
-                <x-form-select
-                    name="company_id"
-                    id="company_id"
-                    :options="$companies"
-                    class="nav-items"
-                    :noDefault="true"
-                    :value="request('company_id')"
-                >Company</x-form-select>
+{{--            <div class="filters">--}}
+{{--                <x-form-select--}}
+{{--                    name="company_id"--}}
+{{--                    id="company_id"--}}
+{{--                    :options="$companies"--}}
+{{--                    class="nav-items"--}}
+{{--                    :noDefault="true"--}}
+{{--                    :value="request('company_id')"--}}
+{{--                >Company</x-form-select>--}}
 
-                <x-form-select
-                    name="status"
-                    id="status"
-                    :options="['active'=> 'Active', 'inactive' => 'Inactive']"
-                    :noDefault="true"
-                    :value="request('status')"
-                >Status</x-form-select>
-            </div>
-        </div>
+{{--                <x-form-select--}}
+{{--                    name="status"--}}
+{{--                    id="status"--}}
+{{--                    :options="['active'=> 'Active', 'inactive' => 'Inactive']"--}}
+{{--                    :noDefault="true"--}}
+{{--                    :value="request('status')"--}}
+{{--                >Status</x-form-select>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
         <div class="btn-link">
             <x-button-link source="employee.register.1" :noDefault="true">Add Employee</x-button-link>
