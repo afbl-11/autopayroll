6c63ff5585444d83846a1c29a75abac8e25ad9a8
Jurell Afable <133481515+afbl-11@users.noreply.github.com>
2026-01-24 18:42:38 +0800

    chore(db): add migration leave_credit_types

---
+++ app/Models/LeaveCreditType.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LeaveCreditType extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'leave_credit_type_id';
+    public $incrementing = false; // because UUID
+    protected $keyType = 'string';
+
+    protected $fillable = ['leave_credit_type_id', 'name'];
+}
